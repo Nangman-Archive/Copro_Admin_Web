09cc95f0d36d9584c0ff8ebbbb14f7b7144c64b8
devpark435 <devpark435@gmail.com>
2024-03-10 20:51:33 +0900

    feat - google signin flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:copro_admin_web/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_sign_in_web_redirect/google_sign_in_web_redirect.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+// late SharedPreferences sharedPreferences;
+
+void main() async {
+  GoogleSignWeb.getQueryParameters();
+
+  ///make sure add this line
+  setPathUrlStrategy();
+
+  ///make sure add this line
   runApp(const MyApp());
 }
 
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({
+    super.key,
+  });
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn(
+    clientId:
+        '103359214072-ch18gni5f09onpqm86nqfhdo2hc692kt.apps.googleusercontent.com',
+  );
+  @override
+  void initState() {
+    super.initState();
+    signInWithGoogle();
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      debugPrint("구글 로그인 버튼 눌림");
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signInSilently(); // Change this line
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        debugPrint("ID Token: ${googleSignInAuthentication.idToken}");
+      } else {
+        debugPrint("No existing user found. Please sign in.");
+      }
+    } catch (error) {
+      debugPrint("Error signing in: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.teal,
+          disabledForegroundColor: Colors.grey.withOpacity(0.38),
+          disabledBackgroundColor: Colors.grey.withOpacity(0.12),
+        ),
+        onPressed: () {
+          // signInWithGoogle();
+        },
+        child: const Text(""),
+      ),
+    ));
   }
 }
