131e6f412ddcf4b43d22ec7583cb3019f565296a
devpark435 <devpark435@gmail.com>
2024-03-12 09:13:45 +0900

    feat - Implement API call for fetching reported items list

---
+++ lib/services/report_services.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:copro_admin_web/models/report_list_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ReportServices {
+  final String apiBaseUrl =
+      dotenv.env['API_BASE_URL'] ?? 'default_value_if_not_present';
+  Future<ReportListModel> getReportList() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? accessToken = prefs.getString('accessToken');
+    var headers = {'Authorization': 'Bearer $accessToken'};
+    var dio = Dio();
+    var response = await dio.request(
+      '$apiBaseUrl/api/admin/report?page=0&size=10',
+      options: Options(
+        method: 'GET',
+        headers: headers,
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint(json.encode(response.data));
+      return ReportListModel.fromJson(response.data);
+    } else {
+      debugPrint(response.statusMessage);
+      return ReportListModel.fromJson(response.data);
+    }
+  }
+}
